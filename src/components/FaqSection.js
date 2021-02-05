8fa65ad2092ceb17bf2a57c11202441636d68a2c
herschelle <herschelle21@gmail.com>
2021-02-05 17:24:14 +0530

    6 faq styled

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,12 +1,19 @@
 // Components
 import React from "react";
+import styled from "styled-components";
 
 // Custom Components
+import {
+  StyledAbout,
+  StyledDescription,
+  StyledHide,
+  StyledImageDiv,
+} from "../Styles";
 
 // Function
 function FaqSection() {
   return (
-    <div className="faq">
+    <StyledFaq>
       <h2>
         Any Questions <span>FAQ</span>
       </h2>
@@ -29,6 +36,7 @@ function FaqSection() {
             distinctio!
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>
       <div className="question">
         <h4>Diffrent payment methods</h4>
@@ -39,6 +47,7 @@ function FaqSection() {
             distinctio!
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>
       <div className="question">
         <h4>What products do you offer</h4>
@@ -49,9 +58,35 @@ function FaqSection() {
             distinctio!
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>
-    </div>
+    </StyledFaq>
   );
 }
-
+const StyledFaq = styled(StyledAbout)`
+  display: block;
+  span {
+    display: block;
+  }
+  h2 {
+    padding-bottom: 2rem;
+    font-weight: lighter;
+  }
+  .faq-line {
+    background: #cccccc;
+    height: 0.2rem;
+    margin: 2rem;
+    width: 100%;
+  }
+  .question {
+    padding: 3rem 0rem;
+    cursor: pointer;
+  }
+  .answer {
+    padding: 3rem 0rem;
+    p {
+      padding: 1rem 0rem;
+    }
+  }
+`;
 export default FaqSection;
